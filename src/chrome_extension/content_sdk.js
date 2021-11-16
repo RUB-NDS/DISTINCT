96b27bfac8f2a15f812423813ae04b2742a3633c
Louis Jannett <louis.jannett@rub.de>
2021-11-16 14:02:31 +0100

    Minor change

--- src/chrome_extension/content_sdk.js
+++ src/chrome_extension/content_sdk.js
@@ -28,8 +28,7 @@ let content_sdk = () => {
     /* Facebook Login SDK */
     /* Docs: https://developers.facebook.com/docs/facebook-login/web */
     if (
-        window.opener
-        && location.host.endsWith("facebook.com") // www.facebook.com, web.facebook.com, ...
+        location.host.endsWith("facebook.com") // www.facebook.com, web.facebook.com, ...
         && location.pathname.endsWith("/dialog/oauth") // /v11.0/dialog/oauth
         && "app_id" in _sso._qparams
         && "display" in _sso._qparams
@@ -45,8 +44,7 @@ let content_sdk = () => {
     /* Google Sign-In SDK (Legacy) */
     /* https://developers.google.com/identity/sign-in/web/sign-in */
     if (
-        window.opener
-        && location.host === "accounts.google.com"
+        location.host === "accounts.google.com"
         && location.pathname === "/o/oauth2/auth"
         && "redirect_uri" in _sso._qparams
         && _sso._qparams["redirect_uri"].startsWith("storagerelay://")
@@ -60,8 +58,7 @@ let content_sdk = () => {
     /* Sign in with Google Button SDK */
     /* Docs: https://developers.google.com/identity/gsi/web/guides/personalized-button */
     if (
-        window.opener
-        && location.host === "accounts.google.com"
+        location.host === "accounts.google.com"
         && location.pathname === "/gsi/select"
         && "ux_mode" in _sso._qparams
         && "ui_mode" in _sso._qparams
@@ -81,7 +78,6 @@ let content_sdk = () => {
         let url = arguments[1] || undefined;
         if (
             url
-            && window.parent
             && url.startsWith("/gsi/issue")
             && url.includes("select_by=user_1ta")
         ) {
