7eec1e0def6e79d3edfa19b7f8a91851747174d0
Louis Jannett <louis.jannett@rub.de>
2022-03-27 00:50:22 +0100

    pretty print postmessage callback

--- src/distinct-core/distinct-gui/src/components/ReportsTableView.vue
+++ src/distinct-core/distinct-gui/src/components/ReportsTableView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="table-responsive mb-4">
-    <table class="table"> <!--table-striped-->
+    <table class="table">
       <thead>
         <tr>
           <th scope="col">ID</th>
@@ -19,12 +19,30 @@
           <td>{{report.key}}</td>
           <td>{{report.val.hierarchy}}</td>
           <td class="href">{{report.val.href}}</td>
-          <td class="td-content">
+
+          <!-- Report Content -->
+          <td>
             <div v-for="(val, key) in this.filterVals(report.val)" v-bind:key="key" class="val">
+
+              <!-- custom layout: html -->
               <span v-if="key == 'html'">
                 <b>{{key}}</b>:
                 <pre class="language-html"><code>{{this.beautifyHTML(val)}}</code></pre>
               </span>
+
+              <!-- custom layout: data (data_type = object) -->
+              <!-- <span v-else-if="key == 'data' && report.val['data_type'] == 'object'">
+                <b>{{key}}</b>:
+                <pre class="language-js"><code>{{this.beautifyJS(JSON.stringify(val))}}</code></pre>
+              </span> -->
+
+              <!-- custom layout: callback (of message event listener) -->
+              <span v-else-if="key == 'callback' && report.key == 'addeventlistener' && report.val['type'] == 'message'">
+                <b>{{key}}</b>:
+                <pre class="language-js"><code>{{this.beautifyJS(val)}}</code></pre>
+              </span>
+
+              <!-- standard -->
               <span v-else>
                 <b>{{key}}</b>: {{val}}
               </span>
@@ -38,7 +56,7 @@
 
 <script>
 import Prism from 'prismjs'
-import html_beautify from 'js-beautify'
+import beautify from 'js-beautify'
 import 'prismjs/themes/prism.css'
 
 export default {
@@ -62,7 +80,7 @@ export default {
     },
     'beautifyHTML': function(html) {
       if (this.prettyPrintHTML) {
-        return html_beautify.html(html, {
+        return beautify.html(html, {
           "indent_size": 2,
           "indent_char": " ",
           "indent_with_tabs": false,
@@ -90,6 +108,38 @@ export default {
         })
       } else {
         return html
+      }
+    },
+    'beautifyJS': function(js) {
+      if (this.prettyPrintHTML) {
+        return beautify.js(js, {
+          "indent_size": 2,
+          "indent_char": " ",
+          "indent_with_tabs": false,
+          "eol": "\n",
+          "end_with_newline": false,
+          "indent_level": 0,
+          "preserve_newlines": true,
+          "max_preserve_newlines": 10,
+          "space_in_paren": false,
+          "space_in_empty_paren": false,
+          "jslint_happy": false,
+          "space_after_anon_function": false,
+          "space_after_named_function": false,
+          "brace_style": "collapse",
+          "unindent_chained_methods": false,
+          "break_chained_methods": false,
+          "keep_array_indentation": false,
+          "unescape_strings": false,
+          "wrap_line_length": 0,
+          "e4x": false,
+          "comma_first": false,
+          "operator_position": "before-newline",
+          "indent_empty_lines": false,
+          "templating": ["auto"]
+        })
+      } else {
+        return js
       }
     }
   },
