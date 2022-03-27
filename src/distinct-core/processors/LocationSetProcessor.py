aa09543f88e90d4ead2a10d559cbc52ee11eb161
Louis Jannett <louis.jannett@rub.de>
2022-03-27 22:22:59 +0200

    update location set processir

--- src/distinct-core/processors/LocationSetProcessor.py
+++ src/distinct-core/processors/LocationSetProcessor.py
@@ -1,7 +1,12 @@
-from urllib import parse
+import logging
+
 from urllib.parse import urlparse
+
 from model.ReportProcessor import ReportProcessor
+from model.ReportAnalysis import ReportAnalysis
 from processors.PostMessageReceivedProcessor import PostMessageReceivedProcessor
+
+logger = logging.getLogger(__name__)
 
 class LocationSetProcessor(ReportProcessor):
     """ LOCATION SET
@@ -11,34 +16,57 @@ class LocationSetProcessor(ReportProcessor):
     def __init__(self, ctx, report):
         super().__init__(ctx, report)
 
-        keyval = {
+        seq_diag_keyval = {
             "Property": self.val["prop"],
             "Target": self.val["target"]
         }
         color = None
 
-        # Check for relative redirects
-        if self.relative_redirect(self.val["prop"], self.val["target"]):
-            keyval["Info"] = (
+        # 1) Check for relative redirects
+        logger.debug(f"Check if location set in report #{self.id} is relative redirect")
+        if ReportAnalysis.location_target_is_relative_redirect(self.val["prop"], self.val["target"]):
+            report["val"]["relative_redirect"] = True
+            seq_diag_keyval["Info"] = (
                 "Detected potential relative redirect. Check if this redirect contains "
                 "confidential data and is performed across windows."
             )
             color="orange"
-
         else:
+            report["val"]["relative_redirect"] = False
 
-            # Check if redirect target comes from user input
-            related_events = self.search_userinput(ctx, self.val["prop"], self.val["target"])
+            # 2) If it is not a relative redirect, check if redirect target comes from user input
+            logger.debug(f"Check if location set in report #{self.id} comes from user input")
+            related_reports = []
 
-            if related_events:
-                keyval["Info"] = (
+            # If the location is set with a method that can (1) change its origin
+            # and (2) include confidential data in query or fragment
+            if (
+                self.val["prop"] == "href"
+                or self.val["prop"] == "assign"
+                or self.val["prop"] == "replace"
+            ):
+                target_parsed = urlparse(self.val["target"])
+                search_url = ""
+                if target_parsed.scheme: search_url += f"{target_parsed.scheme}://"
+                if target_parsed.netloc: search_url += target_parsed.netloc
+                if target_parsed.path: search_url += target_parsed.path
+                # We do not want to search for query or fragment
+
+                if search_url:
+                    related_reports = ReportAnalysis.related_reports_with_url_in_user_input(ctx.reports, search_url)
+
+            related = ", ".join(str(report["id"]) for report in related_reports)
+            report["val"]["related_reports"] = related
+            logger.debug(f"Related reports: {related}")
+
+            if related_reports:
+                seq_diag_keyval["Info"] = (
                     "Detected that the redirect target potentially depends on "
                     "user input. Check if the user input in the related events influences the "
                     "redirect target by actively modifying it."
                 )
-                keyval["Related Events"] = ", ".join(str(processor.id) for processor in related_events)
+                seq_diag_keyval["Related Events"] = related
                 color = "orange"
-
             else:
                 color = "green"
 
@@ -47,68 +75,6 @@ class LocationSetProcessor(ReportProcessor):
             self.id,
             self.timestamp,
             "Location Set",
-            keyval,
+            seq_diag_keyval,
             color = color
         )
-
-    @staticmethod
-    def relative_redirect(property, target):
-        """ Check if the target of the location set event is a relative redirect
-            Returns True, if target is a relative redirect.
-            Returns False, if target is not a relative redirect.
-        """
-
-        # If location is set via href, assign, or replace, and starts with "/", it is relative
-        if (
-            (
-                property == "href"
-                or property == "assign"
-                or property == "replace"
-            )
-            and
-            (
-                target.startswith("/")
-                and not target.startswith("//") # ignore "//example.com" -> "https://example.com"
-            )
-        ):
-            return True
-
-        # If location is set via pathname, it is always a relative redirect
-        elif (property == "pathname"):
-            return True
-
-        else:
-            return False
-
-    @staticmethod
-    def search_userinput(ctx, property, target):
-        """ Search all events in reverse order that can contain user input for the url.
-            Example: If "https://example.com" is used in postMessage or location set,
-            then search all previous events that can contain user input as GET / POST
-            parameters for this url. If some event like "Document Init" contains this url
-            as GET parameter, we want to include this event as a related event to this event.
-
-            Returns a list of related events (event processors).
-        """
-
-        # If the location is set with a method that can
-        # 1) change its origin and
-        # 2) include confidential data in params like query or fragment
-        if (
-            property == "href"
-            or property == "assign"
-            or property == "replace"
-        ):
-            parsed = urlparse(target)
-            search_url = ""
-
-            if parsed.scheme: search_url += f"{parsed.scheme}://"
-            if parsed.netloc: search_url += parsed.netloc
-            if parsed.path: search_url += parsed.path
-            # We do not want to search for the query or fragment, as these probably do not come
-            # from the user input (i.e., the query might contain a token)
-
-            if search_url:
-                return PostMessageReceivedProcessor.search_userinput(ctx.processors, search_url)
-            else:
-                return []
