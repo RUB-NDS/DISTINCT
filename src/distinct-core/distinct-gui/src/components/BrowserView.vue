65f1d38fa2938634b50f80ae31b54cac7b3018c9
Louis Jannett <louis.jannett@rub.de>
2022-04-28 09:30:16 +0200

    dynamic url for browser frame

--- src/distinct-core/distinct-gui/src/components/BrowserView.vue
+++ src/distinct-core/distinct-gui/src/components/BrowserView.vue
@@ -2,14 +2,25 @@
   <div class="container-fluid">
     <h1>Browser</h1>
 
-    <iframe id="browserFrame" src="http://localhost:9090/vnc_auto.html?password=changeme"></iframe>
+    <iframe id="browserFrame"></iframe>
   </div>
 </template>
 
 <script>
 export default {
   name: 'BrowserView',
+  mounted() {
+    let currentURL = new URL(window.location.href)
+    let browserURL = new URL()
+    browserURL.protocol = currentURL.protocol
+    browserURL.host = currentURL.host
+    browserURL.port = '9090'
+    browserURL.pathname = '/vnc_auto.html'
+    // browserURL.searchParams.set('password', 'changeme')
 
+    let browserFrame = document.getElementById('browserFrame')
+    browserFrame.src = browserURL.href
+  }
 }
 </script>
 
