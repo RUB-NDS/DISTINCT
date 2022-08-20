83cb405edcf37229c3e6d3864f4748c3952246cf
Louis <louisjannett@icloud.com>
2022-08-20 18:53:02 +0000

    small fix

--- src/distinct-core/gui/vue.config.js
+++ src/distinct-core/gui/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/static/'
+  publicPath: '/static/',
+  devServer: {
+    allowedHosts: 'all',
+  }
 })
